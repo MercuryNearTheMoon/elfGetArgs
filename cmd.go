2c10eaf23ff20bf3373b8b65713ed2ba669ed08d
mercury <mercuryhuang@qnap.com>
2025-08-26 11:02:43 +0800

    Implement: hint for argument arg

--- cmd.go
+++ cmd.go
@@ -14,7 +14,7 @@ func ParseArgs() (Args, error) {
 	pflag.StringVarP(&a.Path, "path", "p", "", "Target directory or file to scan (required)")
 	pflag.StringVarP(&a.Arch, "arch", "A", "", "Target architecture (amd64, arm64) (required)")
 	pflag.StringArrayVarP(&a.Funcs, "func", "f", []string{}, "Function name to search (can repeat)")
-	pflag.IntSliceVarP(&a.ArgNums, "arg", "a", []int{}, "Argument index for corresponding function (can repeat)")
+	pflag.IntSliceVarP(&a.ArgNums, "arg", "a", []int{}, "Argument index for corresponding function (can repeat, start from 0)")
 	pflag.StringVarP(&a.Out, "out", "o", "", "Output CSV file (optional)")
 	pflag.IntVarP(&a.Worker, "worker", "w", 4, "Number of workers (optional, default 4)")
 
@@ -29,7 +29,6 @@ func ParseArgs() (Args, error) {
 	if len(a.Funcs) != len(a.ArgNums) {
 		return a, errors.New("number of func and arg must match")
 	}
-
 	return a, nil
 }
 
