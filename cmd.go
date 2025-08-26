9e2a672f0f2889b551ed110a68c753b64da7889f
mercury <mercuryhuang@qnap.com>
2025-08-26 12:00:40 +0800

    Implement: Usage prompt

--- cmd.go
+++ cmd.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"os"
 
 	"github.com/spf13/pflag"
 )
@@ -18,16 +18,35 @@ func ParseArgs() (Args, error) {
 	pflag.StringVarP(&a.Out, "out", "o", "", "Output CSV file (optional)")
 	pflag.IntVarP(&a.Worker, "worker", "w", 4, "Number of workers (optional, default 4)")
 
-	pflag.Parse()
+	pflag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n\n", os.Args[0])
+		fmt.Fprintln(os.Stderr, "Required flags:")
+		fmt.Fprintln(os.Stderr, "  -p, --path string       Target directory or file to scan")
+		fmt.Fprintln(os.Stderr, "  -A, --arch string       Target architecture (amd64, arm64)")
+		fmt.Fprintln(os.Stderr, "  -f, --func string       Function name to search (can repeat, at least one required)")
+		fmt.Fprintln(os.Stderr, "  -a, --arg int           Argument index for corresponding function (can repeat, start from 0, at least one required)")
 
+		fmt.Fprintln(os.Stderr, "\nOptional flags:")
+		fmt.Fprintln(os.Stderr, "  -o, --out string        Output CSV file (optional)")
+		fmt.Fprintln(os.Stderr, "  -w, --worker int        Number of workers (optional, default 4)")
+
+		fmt.Fprintln(os.Stderr, "\nExample:")
+		fmt.Fprintf(os.Stderr, "  %s -p ./binaries -A amd64 -f open -a 0 -f strcmp -a 1 -o output.csv -w 8\n", os.Args[0])
+	}
+
+	pflag.Parse()
 	if a.Path == "" || a.Arch == "" {
-		return a, errors.New("path and arch are required")
-	}
-	if len(a.Funcs) == 0 || len(a.ArgNums) == 0 {
-		return a, errors.New("at least one func and arg are required")
-	}
-	if len(a.Funcs) != len(a.ArgNums) {
-		return a, errors.New("number of func and arg must match")
+		fmt.Fprintln(os.Stderr, "Error: path and arch are required")
+		pflag.Usage()
+		os.Exit(1)
+	} else if len(a.Funcs) == 0 || len(a.ArgNums) == 0 {
+		fmt.Fprintln(os.Stderr, "Error: at least one func and arg are required")
+		pflag.Usage()
+		os.Exit(1)
+	} else if len(a.Funcs) != len(a.ArgNums) {
+		fmt.Fprintln(os.Stderr, "Error: number of func and arg must match")
+		pflag.Usage()
+		os.Exit(1)
 	}
 	return a, nil
 }
