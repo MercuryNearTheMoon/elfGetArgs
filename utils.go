a74e4c2c3fdd9bef7310e85478515030b60c91ba
mercury <mercuryhuang@qnap.com>
2025-08-26 10:18:59 +0800

    add parseRegisters

--- utils.go
+++ utils.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+var X86_64Registers = []string{"rdi", "rsi", "rdx", "rcx", "r8", "r9"}
+var ARM64Registers = []string{"x0", "x1", "x2", "x3", "x4", "x5"}
+
 func printArgs(args Args) {
 	fmt.Println("Path   :", args.Path)
 	fmt.Println("Arch   :", args.Arch)
@@ -9,4 +12,25 @@ func printArgs(args Args) {
 	fmt.Println("Args   :", args.ArgNums)
 	fmt.Println("Output :", args.Out)
 	fmt.Println("Worker :", args.Worker)
+}
+
+func parseRegisters(regIdx []int, arch string) ([]string, error) {
+	var (
+		targetRegs []string
+		results    []string
+	)
+
+	switch arch {
+	case "amd64":
+		targetRegs = X86_64Registers
+	case "arm64":
+		targetRegs = ARM64Registers
+	default:
+		return nil, fmt.Errorf("Unsupported Arch Type: %s", arch)
+	}
+
+	for _, idx := range regIdx {
+		results = append(results, targetRegs[idx])
+	}
+	return results, nil
 }
